b101edb8e24577e4e6747f1d537832914e1dace0
JohnGoure <johnsgithub@gmail.com>
2018-05-08 22:06:02 -0500

    Initial commit

---
+++ Login/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by john goure on 5/8/18.
+//  Copyright © 2018 john goure. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
